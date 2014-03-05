54432a0813f332f31900eb4527eac0645881f07b
Joshua McKenty <joshua@pistoncloud.com>
2014-03-05 12:33:05 -0800

    Confirm that they've sourced credentials
    Change-Id: Ib007ecb179c48f0dfc64d11fe720b801762c5690

--- kick.py
+++ kick.py
@@ -4,6 +4,11 @@ import os
 import subprocess
 
 if __name__ == "__main__":
-	#TODO(JMC): Consider using PIPE instead, ala 
-	# http://stackoverflow.com/questions/13332268/python-subprocess-command-with-pipe
+    
+    # Confirm that they've sourced their openrc credentials already
+    if not os.environ.get('OS_PASSWORD'):
+        raise Exception('You need to source your openrc file first.')
+    
+    #TODO(JMC): Consider using PIPE instead, ala 
+    # http://stackoverflow.com/questions/13332268/python-subprocess-command-with-pipe
     subprocess.check_output("docker build .".split(" "), env=os.environ)
